9bb6c5820cca74bf4dad50579919ae8ef432e59c
Monu Prajapat <monuprajapat6270@gmail.com>
2023-12-14 15:24:38 +0530

    adding icon

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import './App.css'
 import { AddToDo } from './components/AddToDo'
 import { Navbar } from './components/Navbar';
 import { Typography } from 'antd';
+import {CarryOutOutlined} from '@ant-design/icons';
 
 const { Title } = Typography;
 function App() { 
   return (
     <>
       <main>
-       <Title level={2}>Todo with React + TypeScript</Title>
+       <Title level={2}>Todo with React + TypeScript <CarryOutOutlined /></Title>
         <AddToDo />
         <Navbar />
       </main>
